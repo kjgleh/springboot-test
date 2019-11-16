3b12cd7e8f716e1d8ca49c0111f4d5478086811a
김정근 <gogoogjg@tripstore.kr>
2019-11-16 14:06:49 +0900

    initial commit

---
+++ src/main/kotlin/me/kjgleh/springbootTest/SpringbootTestApplication.kt
@@ -0,0 +1,11 @@
+package me.kjgleh.springbootTest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootTestApplication>(*args)
+}
---
+++ src/test/kotlin/me/kjgleh/springbootTest/SpringbootTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.kjgleh.springbootTest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
